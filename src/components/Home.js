398ab08956e8659a030554c41a9cb1440d79d1f6
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-05-20 08:14:19 -0700

    setup home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import '../styles/Home.css';
 
+import image from "../images/animation2.png"
+
 function Home() {
   return (
     <div className="home-container">
-      
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <h1 className='intro-head'>Hi, im Blake <br/>front-end software engineer</h1>
+                </div>
+                <div className="col-6">
+                    <img src={image}></img>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
