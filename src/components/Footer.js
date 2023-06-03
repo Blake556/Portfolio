c58beb9b0330e52a383357e701ef0ec459dfd54f
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-06-03 13:29:32 -0700

    After secondary color change for entire website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,28 @@ import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-
 function Footer() {
+  const iconStyle = {
+    color: 'limegreen',
+    marginLeft: '40px',
+  };
+
   return (
     <div className="footer-container d-flex align-items-center">
       <div className="">
-      <a href="https://github.com/Blake556?tab=repositories" target="blank"><FontAwesomeIcon icon={faGithub} size="2x" className="github-icon text-primary" /></a>
-      <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="blank"><FontAwesomeIcon icon={faEnvelope} size="2x" className="github-icon text-primary"/>  </a>
-      <a ><FontAwesomeIcon icon={faLinkedin} size="2x" className="github-icon text-primary"/> </a>
-      <a ><FontAwesomeIcon icon={faFacebookF} size="2x" className="github-icon text-primary"/> </a>
+        <a href="https://github.com/Blake556?tab=repositories" target="_blank">
+          <FontAwesomeIcon icon={faGithub} size="2x" style={iconStyle} />
+        </a>
+        <a href="https://mail.google.com/mail/u/0/#inbox?compose=new" target="_blank">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" style={iconStyle} />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faLinkedin} size="2x" style={iconStyle} />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faFacebookF} size="2x" style={iconStyle} />
+        </a>
       </div>
-     
     </div>
   );
 }
